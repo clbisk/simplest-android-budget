75daf8a651840db6d892e5c2539d15ea58daf721
clbisk <clbisk@gmail.com>
2024-12-11 15:13:22 -0500

    fix androidstudio warnings

--- simplest_budget/app/build.gradle.kts
+++ simplest_budget/app/build.gradle.kts
@@ -42,21 +42,21 @@ android {
 
 dependencies {
 	// Compose
-	implementation(platform("androidx.compose:compose-bom:2024.11.00"))
-	implementation("androidx.activity:activity-compose:1.9.3")
-	implementation("androidx.compose.material3:material3")
-	implementation("androidx.compose.ui:ui")
-	implementation("androidx.compose.ui:ui-tooling")
-	implementation("androidx.compose.ui:ui-tooling-preview")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-	implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-	implementation("androidx.navigation:navigation-compose:2.8.4")
+	implementation(platform(libs.androidx.compose.bom))
+	implementation(libs.androidx.activity.compose)
+	implementation(libs.material3)
+	implementation(libs.ui)
+	implementation(libs.ui.tooling)
+	implementation(libs.ui.tooling.preview)
+	implementation(libs.androidx.lifecycle.runtime.ktx)
+	implementation(libs.androidx.lifecycle.viewmodel.compose)
+	implementation(libs.androidx.navigation.compose)
 
 	// Room
-	implementation("androidx.room:room-runtime:2.6.1")
-	implementation("androidx.core:core-ktx:1.15.0")
-	ksp("androidx.room:room-compiler:2.6.1")
-	implementation("androidx.room:room-ktx:2.6.1")
+	implementation(libs.androidx.room.runtime)
+	implementation(libs.androidx.core.ktx)
+	ksp(libs.androidx.room.compiler)
+	implementation(libs.androidx.room.ktx)
 
 	// included with new empty activity
 	implementation(libs.androidx.core.ktx)
